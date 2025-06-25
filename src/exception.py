d339518ce53b62095c66c72034b6759d54fddf7e
mdurgasaianil <durgaanil7@gmailcom>
2025-06-25 13:35:53 +0530

    Added logging

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,8 @@
 import sys # python function provides various functions and variables that are used to manipulate different parts of the python runtime.
 # sys library will conains the information of exception content.
 
+from src.logger import logging
+
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info() # this funcion will give you details like on which file and which line etc.. the exception occured.
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -21,4 +23,5 @@ class CustomException(Exception):
 #     try:
 #         a=1/0
 #     except Exception as e:
+#         logging.info(str(e))
 #         raise CustomException(e,sys)
