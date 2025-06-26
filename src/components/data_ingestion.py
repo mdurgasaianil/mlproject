c93c12d0895135e17beeb40606ece86274a8788f
mdurgasaianil <durgaanil7@gmailcom>
2025-06-26 21:12:38 +0530

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,10 +8,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass  # this used to create an class variable
 
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 # any input that is required while doing data injestion that we will take input from below class
 @dataclass
 class DataIngestionConfig:
-    # Since we using the data class decorator we were no need of creatin __init__ for creating a class
+    # Since we using the data class decorator we were no need of creating __init__ for creating a class
     # we can directly creata class
     train_data_path: str = os.path.join('artifacts',"train.csv") # all outputs will be saved under artifact folder, so here the train data will be stored in this path
     test_data_path: str = os.path.join('artifacts',"test.csv") 
@@ -46,7 +51,9 @@ class DataIngestion:
             
 if __name__=='__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
     
 
 
