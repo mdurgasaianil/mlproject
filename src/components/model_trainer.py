bba1c34985a924b6bc2ef4f4ee41ce9fb0a5ce1b
mdurgasaianil <durgaanil7@gmailcom>
2025-06-27 11:10:46 +0530

    Hyper parameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -39,7 +39,7 @@ class ModelTrainer:
 
             models = {
                 "LinearRegression":LinearRegression(),
-                "KNeighborsRegressor":KNeighborsRegressor(),
+                #"KNeighborsRegressor":KNeighborsRegressor(),
                 "DecisionTreeRegressor":DecisionTreeRegressor(),
                 "RandomForestRegressor":RandomForestRegressor(),
                 "XGBRegressor": XGBRegressor(),
@@ -47,7 +47,45 @@ class ModelTrainer:
                 "AdaBoostRegressor":AdaBoostRegressor()
             }
 
-            model_report:dict=evaluate_models(x_train,y_train,x_test,y_test,models=models)
+            params={
+                "DecisionTreeRegressor": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "RandomForestRegressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "LinearRegression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoostRegressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoostRegressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+            model_report:dict=evaluate_models(x_train,y_train,x_test,y_test,models=models,params=params)
 
             # best_model_name = max(model_report, key=model_report.get)
 
